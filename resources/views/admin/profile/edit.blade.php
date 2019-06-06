793c42ee6923f51965244525a76f6172d9e66b4d
masami tawara <masamix.c0m@hotmail.co.jp>
2019-06-06 13:08:14 +0000

    14th commit

--- resources/views/admin/profile/edit.blade.php
+++ resources/views/admin/profile/edit.blade.php
@@ -50,8 +50,8 @@
                          <div class="col-md-4 mx-auto">
                         <h2>更新履歴</h2>
                         <ul class="list-group">
-                            @if ($profile_form->Histories != NULL)
-                                @foreach ($profile_form->Histories as $history)
+                            @if ($profile_form->histories != NULL)
+                                @foreach ($profile_form->histories as $history)
                                     <li class="list-group-item">{{ $history->edited_at }}</li>
                                 @endforeach
                             @endif
