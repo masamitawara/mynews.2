677abb57c75247b764b3342d95a224935e65d220
masami tawara <masamix.c0m@hotmail.co.jp>
2019-06-07 13:48:44 +0000

    15th commit

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -6,15 +6,15 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\HTML;
 use App\News;
 
-class NewsController extends Controller
+class ProfileController extends Controller
 {
     public function index(Request $request)
     {
-        $cond_title = $request->cond_title;
+        $cond_name = $request->cond_name;
         
         // $cond_title が空白でない場合は、記事を検索して取得する
-        if ($cond_title != '') {
-            $posts = News::where('title', $cond_title).orderBy('updated_at', 'desc')->get();
+        if ($cond_name != '') {
+            $posts = News::where('name', $cond_name).orderBy('updated_at', 'desc')->get();
         } else {
             $posts = News::all()->sortByDesc('updated_at');
         }
@@ -28,8 +28,6 @@ class NewsController extends Controller
         // news/index.blade.php ファイルを渡している
         // また View テンプレートに headline、 posts、 cond_title という変数を渡している
         return view('news.index', 
-        ['headline' => $headline, 'posts' => $posts,'cond_title' => $cond_title]);
-        
-        
+        ['headline' => $headline, 'posts' => $posts,'cond_name' => $cond_name]);
     }
 }
